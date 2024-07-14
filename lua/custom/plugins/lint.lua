74ae12a9a2d558bb88b149c540917567657ceacd
Vuko Vukasinovic <vukasinovicvuko5@gmail.com>
2024-07-14 12:07:17 +0200

    updates

--- lua/custom/plugins/lint.lua
+++ lua/custom/plugins/lint.lua
@@ -6,6 +6,7 @@ return { -- Linting
       local lint = require 'lint'
       lint.linters_by_ft = {
         markdown = { 'markdownlint' },
+        json = { 'jsonlint' },
         javascript = { 'eslint_d' },
         typescript = { 'eslint_d' },
         javascriptreact = { 'eslint_d' },
--- lua/custom/plugins/which-key.lua
+++ lua/custom/plugins/which-key.lua
@@ -1,22 +1,64 @@
-return { -- Useful plugin to show you pending keybinds.
+return {
   'folke/which-key.nvim',
-  event = 'VimEnter', -- Sets the loading event to 'VimEnter'
-  config = function() -- This is the function that runs, AFTER loading
-    require('which-key').setup()
+  event = 'VeryLazy',
 
-    -- Document existing key chains
-    require('which-key').register {
-      ['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },
-      ['<leader>d'] = { name = '[D]ocument', _ = 'which_key_ignore' },
-      ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
-      ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
-      ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
-      ['<leader>t'] = { name = '[T]oggle', _ = 'which_key_ignore' },
-      ['<leader>h'] = { name = 'Git [H]unk', _ = 'which_key_ignore' },
-    }
-    -- visual mode
-    require('which-key').register({
-      ['<leader>h'] = { 'Git [H]unk' },
-    }, { mode = 'v' })
-  end,
+  dependencies = {
+    'nvim-tree/nvim-web-devicons',
+  },
+  opts = {
+    -- Your configuration options can go here
+    presets = true, -- Enable the default presets
+    -- Configure any other options as needed
+  },
+  keys = {
+    {
+      '<leader>?', -- Key sequence to trigger which-key
+      function()
+        require('which-key').show { global = false }
+      end,
+      desc = 'Buffer Local Keymaps (which-key)',
+    },
+    {
+      '<leader>c',
+      "<cmd>echo '[C]ode'<CR>",
+      desc = '[C]ode',
+      mode = 'n',
+    },
+    {
+      '<leader>d',
+      "<cmd>echo '[D]ocument'<CR>",
+      desc = '[D]ocument',
+      mode = 'n',
+    },
+    {
+      '<leader>r',
+      "<cmd>echo '[R]ename'<CR>",
+      desc = '[R]ename',
+      mode = 'n',
+    },
+    {
+      '<leader>s',
+      "<cmd>echo '[S]earch'<CR>",
+      desc = '[S]earch',
+      mode = 'n',
+    },
+    {
+      '<leader>w',
+      "<cmd>echo '[W]orkspace'<CR>",
+      desc = '[W]orkspace',
+      mode = 'n',
+    },
+    {
+      '<leader>t',
+      "<cmd>echo '[T]oggle'<CR>",
+      desc = '[T]oggle',
+      mode = 'n',
+    },
+    {
+      '<leader>h',
+      "<cmd>echo 'Git [H]unk'<CR>",
+      desc = 'Git [H]unk',
+      mode = 'n',
+    },
+  },
 }
